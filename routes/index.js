e85fbd4ec6e7545d76e7b2e01d7dc8b0ff5585ef
BrunoDKoch <bruno.d.koch@gmail.com>
2022-07-14 09:02:53 -0300

    Add messages

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Paulo",
+        added: new Date()
+    },
+    {
+        text: "Hello world!",
+        user: "Carlos",
+        added: new Date()
+    }
+];
 /* GET home page. */
 router.get("/", (req, res, next) => {
     res.render("index", { title: "Express" });
--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import ejs from 'ejs'
 const router = express.Router();
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Paulo",
+        added: new Date()
+    },
+    {
+        text: "Hello world!",
+        user: "Carlos",
+        added: new Date()
+    }
+];
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
